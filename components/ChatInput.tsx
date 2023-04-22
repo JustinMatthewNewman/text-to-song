8361299c6fff5bdae701290ad1900bdf493cc44a
JustinMatthewNewman <newma4jm@dukes.jmu.edu>
2023-04-22 14:55:37 -0400

    Fix prompt bug?

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -6,6 +6,7 @@ import { useSession } from "next-auth/react";
 import React, { useState } from "react";
 import { toast } from "react-hot-toast";
 import useSWR from "swr";
+import voiceData from "./voices.json"
 
 import ModelSelection from "./ModelSelection";
 
@@ -13,25 +14,67 @@ type Props = {
   chatId: string;
 };
 
+type Voice = {
+  added_at: number | null;
+  architecture: string;
+  category: string;
+  contributors: string[];
+  controls: boolean;
+  display_name: string;
+  is_active: boolean;
+  model_id: string;
+  memberships: { name: string; id: number }[];
+  is_private: boolean;
+  is_primary: boolean;
+  name: string;
+  symbol_set: string;
+  voicemodel_uuid: string;
+  hifi_gan_vocoder: string;
+  ml_model_id: number;
+  speaker_id: number | null; // Update this line
+  language: string;
+};
+
+
+
+
 function ChatInput({ chatId }: Props) {
   const { data: session } = useSession();
   const [prompt, setPrompt] = useState("");
   const [loading, setIsLoading] = useState(true);
+  const [voices, setVoices] = useState([]);
+
+  const [selectedArtist, setSelectedArtist] = useState<Voice | null>(null);
 
   const { data: model } = useSWR("model", {
     fallbackData: "text-davinci-003",
   });
 
+  const findVoiceIndex = (voice: Voice) => {
+    return getVoices().findIndex((v) => v.voicemodel_uuid === voice.voicemodel_uuid);
+  };
+  
+
+  const getVoices = () => {
+    return voiceData.filter(voice => voice.category.toLowerCase() === "rappers");
+  };
+
+  const handleVoiceSelection = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedIndex = parseInt(e.target.value);
+    setSelectedArtist(getVoices()[selectedIndex]);
+  };
+  
+
   const generateResponse = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     try {
-      if (!prompt && !session) return;
-
-      const input = prompt.trim();
+      console.log(selectedArtist)
+      if ((!prompt && !session)||(!selectedArtist)) return;
+      const input = "Write a song in the voice of " + selectedArtist.name + " rapping about " + prompt.trim() + ".";
       setPrompt("");
-
       setIsLoading(false);
+
 
       const message: Message = {
         text: input,
@@ -55,7 +98,7 @@ function ChatInput({ chatId }: Props) {
       );
 
       // loading
-      const notification = toast.loading("ChatGPT is thinking...");
+      const notification = toast.loading("Processing...");
 
       await fetch("/api/askQuestion", {
         method: "POST",
@@ -63,35 +106,97 @@ function ChatInput({ chatId }: Props) {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          prompt: input,
+          prompt: "Write a song about beans",
           chatId,
           model,
           session,
         }),
       }).then(() => {
         // Tost Notification
-        toast.success("ChatGPT has responded!", {
+        toast.success("Lyrics!", {
           id: notification,
         });
 
         setIsLoading(true);
       });
     } catch (error: any) {
-      console.log("HERE")
       console.log(error.message);
     }
   };
 
+  // console.log(selectedArtist)
+
   return (
     <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm">
-      <form onSubmit={generateResponse} className="p-5 space-x-5 flex">
+
+
+
+
+
+<div className="mt-10 text-center text-white">
+<img
+        src="https://drive.google.com/uc?export=download&id=1fj7OO7X2uFYOI0zM5ZfjYB27vdI43qEr"
+        alt="logo"
+        className="mx-auto p-4 App-logo"
+      />
+        <label htmlFor="rapper-select">Write a song in the voice of: </label>
+      <div className="p-2">
+        <div>
+<select
+  style={{
+    backgroundColor: "#353942",
+    color: "#fff",
+    borderRadius: "4px",
+    padding: "4px",
+    width: "230px",
+  }}
+  value={selectedArtist ? findVoiceIndex(selectedArtist) : ""}
+  onChange={handleVoiceSelection}
+>
+  <option value="" disabled>
+    Select
+  </option>
+  {getVoices().map((voice, index) => (
+    <option key={voice.voicemodel_uuid} value={index}>
+      {voice.display_name}
+    </option>
+  ))}
+</select>
+
+
+        </div>
+      </div>
+      <br />
+
+      <label htmlFor="song-topic-input">Singing about: </label>
+      <input
+        type="text"
+        placeholder="Type here..."
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        disabled={!session}
+        autoComplete="off"
+        
+        style={{
+          backgroundColor: "#353942",
+          color: "#fff",
+          borderRadius: "4px",
+          padding: "4px",
+        }}
+      />
+
+    </div>
+
+
+
+
+
+
+
+      <form onSubmit={generateResponse} className="p-5 text-center">
         <input
-          type="text"
-          placeholder="Type your message here..."
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
-          disabled={!session}
-          className={`bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-300 ${
+          
+          className={`bg-transparent focus:outline-none flex-1 disabled:cursor-not-allowed disabled:text-gray-700 ${
             !loading && "animate-pulse"
           }`}
         />
@@ -100,49 +205,23 @@ function ChatInput({ chatId }: Props) {
           <button
             type="submit"
             disabled={!prompt || !session}
-            className="bg-[#11A37F] hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:bg-gray-300 disabled:cursor-not-allowed"
+            className="button text-white font-bold px-4 py-2 rounded disabled:bg-gray-700 disabled:cursor-not-allowed"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-4 h-4 -rotate-45"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-              />
-            </svg>
+            Create
           </button>
         ) : (
           <button
             type="submit"
             disabled={!session}
-            className="bg-[#11A37F] hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:bg-gray-300 disabled:cursor-not-allowed"
+            className="bg-[#434343] hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:bg-gray-700 disabled:cursor-not-allowed"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6 animate-spin"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
-              />
-            </svg>
+           Create
           </button>
         )}
       </form>
-      <div className="md:hidden">
+      {/* <div className="md:hidden">
         <ModelSelection />
-      </div>
+      </div> */}
     </div>
   );
 }
